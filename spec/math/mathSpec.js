45a97cc50cdc27fbf09e7a56ed281e94297114cb
zjy <578837197@qq.com>
2017-03-24 15:22:26 +0800

    minus

--- spec/math/mathSpec.js
+++ spec/math/mathSpec.js
@@ -1,12 +1,16 @@
 describe('math',function(){
     // case 1
-    var add = require('../../lib/math/math.js');
+    var math = require('../../lib/math/math.js');
     it('should return the sum of two operands',function(){
-        
+    
         // Give and When
-        var sum = add(1,2);
+        var sum = math.add(1,2);
         //then
         expect(sum).toEqual(3);
 
+    })
+    it('should return the substraction of operands',function(){
+        var substraction = math.minus(3,2);
+        expect(substraction).toEqual(1);
     })
 })
